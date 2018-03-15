439e30bbe947db0abbc6afa863d335e0668add8e
AnkushMalik <ankushmalik631@gmail.com>
2018-03-15 14:06:21 +0530

    add startup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'startups/index'
-
-  get 'startups/show'
-
-  get 'startups/edit'
-
   devise_for :users
   root 'home#index'
 
+  unauthenticated :user do
+    root to: 'home#index'
+  end
+
+  authenticated :user do
+    root to: 'home#feed'
+  end
+
+  resources :startups
+  resources :founderships
+  resources :users
 end
