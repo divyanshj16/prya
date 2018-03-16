3f58633ac89de4b235ea53d831e1b79ee8a5a7e7
AnkushMalik <ankushmalik631@gmail.com>
2018-03-16 14:09:25 +0530

    Seed update for sustages entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Sustage.create(stage: 'idea')
+Sustage.create(stage: 'prototype_ready')
+Sustage.create(stage: 'inmarket')
