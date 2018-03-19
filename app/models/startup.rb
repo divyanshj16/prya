edee5320ab8b823512f03ccd0ed8db121e6f8b2e
AnkushMalik <ankushmalik631@gmail.com>
2018-03-19 13:14:13 +0530

    some changes i dont know

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -2,10 +2,10 @@ class Startup < ApplicationRecord
 
   belongs_to :sustage
 
+  has_many :users, through: :foundership
   has_many :founderships
-  has_many :users, through: :founderships
 
   has_many :startup_interests
-  belongs_to :tag, through: :startup_interests
-  
+  has_many :tag, through: :startup_interests
+
 end
