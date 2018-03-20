03c42729b9b3402cb6e2d067e3f59329bf5acc7e
AnkushMalik <ankushmalik631@gmail.com>
2018-03-20 21:27:53 +0530

    tweaked sidenav

--- app/assets/javascripts/inits.js
+++ app/assets/javascripts/inits.js
@@ -5,7 +5,7 @@ $(function () {
       constrainWidth: false, // Does not change width of dropdown to that of the activator
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      alignment: 'right', // Displays dropdown with edge aligned to the left of button
       stopPropagation: false // Stops event propagation
     }
   );
