5d90944bbc37d9e202ac5588108596b013845350
AnkushMalik <ankushmalik631@gmail.com>
2018-03-21 12:46:20 +0530

    add user covph to profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  has_attached_file :covph, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :covph, styles: { landscape: "700x300#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :covph, content_type: /\Aimage\/.*\z/
 
   has_many :founderships, dependent: :destroy
