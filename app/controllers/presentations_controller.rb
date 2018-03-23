4d8e195f6287bd2ce9a105948123d09cdd6a997a
Divyansh Jha <divyanshj.16@gmail.com>
2018-03-23 16:03:43 +0530

    make structure for presententation#show

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -61,7 +61,7 @@ class PresentationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_ppt
-      @ppt = Presentation.find(params[:id])
+      # @ppt = Presentation.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
